837e0b471cc5efff342be2cbb5c69791976c3398
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-12 18:31:01 +0200

    FizzBuzz Console
    FizzBuzz challange logic with adjustable parameters to divide

--- FizzBuzzChall/Program.cs
+++ FizzBuzzChall/Program.cs
@@ -1,1 +1,38 @@
-﻿
+
+//FizzBuzz(0, 120);
+
+void FizzBuzz(int lowerBound, int upperBound)
+{
+    Dictionary<int, string> rules = new();
+    rules.Add(3, "Fizz");
+    rules.Add(5, "Buzz");
+    rules.Add(7, "Jazz");
+
+    for (int i = lowerBound; i <= upperBound; i++)
+    {
+        string result = "";
+
+        if (i == 0)
+        {
+            Console.WriteLine(0);
+            continue;
+        }
+
+        foreach (var rule in rules)
+        {
+            if (i % rule.Key == 0)
+            {
+                result += rule.Value;
+            }
+        }
+
+        if (result.Length > 0)
+        {
+            Console.WriteLine($"{result} ({i})");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
