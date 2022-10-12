1b5906dfc0515a813731fc8473940ad99a184da6
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-12 18:31:34 +0200

    FizzBuzz reusable refactor
    FizzBuzz refactor to use in any UI

---
+++ FizzBuzzChall/DivisableRuleInfo.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct DivisableRuleInfo
+{
+    public string WordToPrint;
+    public int[] MustBeDivisableBySet;
+
+
+    public DivisableRuleInfo(string word, int[] numberSet)
+    {
+        WordToPrint = word;
+        MustBeDivisableBySet = numberSet;
+    }
+}
--- FizzBuzzChall/Program.cs
+++ FizzBuzzChall/Program.cs
@@ -1,5 +1,25 @@
+﻿
 
 //FizzBuzz(0, 120);
+
+
+List<DivisableRuleInfo> rules = new();
+rules.Add(new DivisableRuleInfo("FizzBuzzJazz", new[] { 3, 5, 7 }));
+rules.Add(new DivisableRuleInfo("FizzBuzz", new[] { 3, 5 }));
+rules.Add(new DivisableRuleInfo("FizzJazz", new[] { 3, 7 }));
+rules.Add(new DivisableRuleInfo("BuzzJazz", new[] { 5, 7 }));
+rules.Add(new DivisableRuleInfo("Fizz", new[] { 3 }));
+rules.Add(new DivisableRuleInfo("Buzz", new[] { 5 }));
+rules.Add(new DivisableRuleInfo("Jazz", new[] { 7 }));
+
+var results = FizzBuzzRefactor(rules, 0, 120);
+
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
+
+
 
 void FizzBuzz(int lowerBound, int upperBound)
 {
@@ -35,4 +55,49 @@ void FizzBuzz(int lowerBound, int upperBound)
             Console.WriteLine(i);
         }
     }
+}
+
+bool NumIsDivisableBy(int num, int[] values)
+{
+    bool output = true;
+    for (int i = 0; i < values.Length; i++)
+    {
+        if (num % values[i] != 0)
+        {
+            output = false;
+            break;
+        }
+    }
+    return output;
+}
+
+List<string> FizzBuzzRefactor(List<DivisableRuleInfo> rules, int lowerBound, int upperBound)
+{
+    List<string> output = new();
+    for (int i = lowerBound; i < upperBound; i++)
+    {
+        bool isFound = false;
+
+        if (i == 0)
+        {
+            output.Add("0");
+            continue;
+        }
+
+        foreach (var rule in rules)
+        {
+            if (NumIsDivisableBy(i, rule.MustBeDivisableBySet))
+            {
+                output.Add($"{rule.WordToPrint} ({i})");
+                isFound = true;
+            }
+        }
+
+        if (isFound == false)
+        {
+            output.Add(i.ToString());
+        }
+
+    }
+    return output;
 }
